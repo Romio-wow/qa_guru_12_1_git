5888213b8ffab60f5991dff08f867c0e416f2c97
Roman Kravchenko <roman.me@mail.ru>
2022-03-31 15:20:58 +0300

    new message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, я почти авто тестировщик");
+        System.out.println("Новое сообщение");
     }
 }
