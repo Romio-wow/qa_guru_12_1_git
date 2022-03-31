b1d7abb630605f5def67bbf8ef21f76b01bcf953
Roman Kravchenko <roman.me@mail.ru>
2022-03-31 18:05:25 +0300

    Switch to UC

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Slow Branch");
 
     }
-    public static void another() {
-        System.out.println("QA Guru");
+    public static void another()
+    {System.out.println("QA GURU");
     }
 }
